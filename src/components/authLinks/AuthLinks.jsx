3664876c2abba9c21faeaa8697c985296ef6be8b
Thiago <thiagodelgado.dev@gmail.com>
2024-03-21 16:39:23 -0300

    Mobile navigation & feature section

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,17 +1,46 @@
+"use client";
 import Link from "next/link";
 import styles from "./authLinks.module.css";
+import { useState } from "react";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState()
   const status = "no authenticated";
   return (
     <>
-      {status === "no authenticated" ? (
-        <Link href={'/'}>Login</Link>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link src="/write">Write</Link>
-          <span className={styles.link}>Logout</span>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link} onClick={() => console.log('hello')}>
+            Logout
+          </span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Lorem ipsum dolor sit amet consectetur</b>, adipisicing elit?
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="Image" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Perspiciatis, veritatis.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            illo enim quis magnam sed possimus porro culpa qui, deserunt
+            temporibus? Corrupti nihil, quaerat omnis eum sequi sunt quasi
+            reprehenderit recusandae.
+          </p>
+          <button className={styles.button}>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
