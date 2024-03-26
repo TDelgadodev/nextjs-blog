a9c3e546df0143b49af1949a066eff971c4f5184
Thiago <thiagodelgado.dev@gmail.com>
2024-03-26 12:25:31 -0300

    Fix details on routes and Add write page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
-import Featured from "@/components/featured/Featured";
+import Featured from "../components/featured/Featured";
 import styles from "./homepage.module.css";
-import CategoryList from "@/components/categoryList/CategoryList";
-import CardList from "@/components/cardList/CardList";
-import Menu from "@/components/menu/Menu";
+import CategoryList from "../components/categoryList/CategoryList";
+import CardList from "../components/cardList/CardList";
+import Menu from "../components/menu/Menu";
 
 export default function Home() {
   return (
